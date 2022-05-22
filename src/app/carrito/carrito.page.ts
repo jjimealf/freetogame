57b05dc8cf7a03c615725c7b1274b2e4be477553
juanmajimenezalf <jimenez.aljua20@cadiz.salesianos.edu>
2022-05-22 19:00:45 +0200

    pdf del pedido funciona en el navegador

--- src/app/carrito/carrito.page.ts
+++ src/app/carrito/carrito.page.ts
@@ -1,6 +1,10 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { ModalController } from '@ionic/angular';
 import { juego } from '../interfaces/interface';
+import * as pdfMake from "pdfmake/build/pdfmake";
+import * as pdfFonts from 'pdfmake/build/vfs_fonts';
+
+(<any>pdfMake).vfs = pdfFonts.pdfMake.vfs;
 
 @Component({
   selector: 'app-carrito',
@@ -10,7 +14,7 @@ import { juego } from '../interfaces/interface';
 export class CarritoPage implements OnInit {
 
   @Input() carrito: juego[];
-
+  pdfObj = null;
   constructor(private modalCtrl: ModalController) { }
 
   ngOnInit() {
@@ -20,7 +24,55 @@ export class CarritoPage implements OnInit {
     this.modalCtrl.dismiss();
   }
 
-  realizarPedido() {
+  getBase64ImageFromURL(url) {
+    return new Promise((resolve, reject) => {
+      var img = new Image();
+      img.setAttribute("crossOrigin", "anonymous");
     
+      img.onload = () => {
+        var canvas = document.createElement("canvas");
+        canvas.width = img.width;
+        canvas.height = img.height;
+    
+        var ctx = canvas.getContext("2d");
+        ctx.drawImage(img, 0, 0);
+    
+        var dataURL = canvas.toDataURL("image/png");
+    
+        resolve(dataURL);
+      };
+    
+      img.onerror = error => {
+        reject(error);
+      };
+    
+      img.src = url;
+    });}
+    
+    
+  async realizarPedido() {
+    var rows = [];
+
+    for(let i=0; i<this.carrito.length; i++) {
+      rows.push(['Titulo: '+this.carrito[i].title]);
+      rows.push(['Descripcion: '+this.carrito[i].short_description]);
+      rows.push(['Desarrollador: '+this.carrito[i].developer]);
+      rows.push(['Plataformas: '+this.carrito[i].platform]);
+    }
+    const pdf = {
+      content: [
+        {
+          image: await this.getBase64ImageFromURL("/assets/logo.png"),
+          alignment: 'center',
+          margin: [0, 0, 0, 20]
+        },
+        {
+          table: {
+            body: rows
+          }
+        }
+      ]
+    }
+    this.pdfObj = pdfMake.createPdf(pdf).download('pedido.pdf');
   }
 }
