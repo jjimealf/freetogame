0a8139db8bc9a0109c079119fa16138941cb94c6
juanmajimenezalf <jimenez.aljua20@cadiz.salesianos.edu>
2022-05-24 01:06:23 +0200

    añadido pestaña grafica

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -30,7 +30,11 @@ const routes: Routes = [
   {
     path: 'favoritos',
     loadChildren: () => import('./favoritos/favoritos.module').then( m => m.FavoritosPageModule)
+  },  {
+    path: 'grafica',
+    loadChildren: () => import('./grafica/grafica.module').then( m => m.GraficaPageModule)
   },
+
 
 
 ];
---
+++ src/app/grafica/grafica.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-grafica',
+  templateUrl: './grafica.page.html',
+  styleUrls: ['./grafica.page.scss'],
+})
+export class GraficaPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
